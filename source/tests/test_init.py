009c7e7b5046f2bd4ac9563c05afa2946b81ef90
Dmitry Dorofeev <dmitry@MacBook-Air-Dmitry.local>
2015-03-17 09:12:45 +0300

    utils 100%

--- source/tests/test_init.py
+++ source/tests/test_init.py
@@ -5,7 +5,6 @@ import lib
 
 
 class InitTestCase(unittest.TestCase):
-
     def test_to_unicode_ansi(self):
         self.assertEqual(lib.to_unicode(chr(88)), 'X')
 
@@ -18,3 +17,16 @@ class InitTestCase(unittest.TestCase):
     def test_prepare_url_str(self):
         with mock.patch('lib.urlunparse', mock.Mock(return_value='somevalue')):
             self.assertEqual(lib.prepare_url('url'), 'somevalue')
+
+    def prepare_url_err(self):
+        with mock.patch("lib.urlparse", mock.Mock(return_value=(None, u'.', None, None, None, None))):
+            with mock.patch("lib.quote", mock.Mock()):
+                with mock.patch("lib.quote_plus", mock.Mock()):
+                    with mock.patch("lib.urlunparse", mock.Mock(return_value='somevalue')):
+                        self.assertEqual(lib.prepare_url('url'), 'somevalue')
+
+    def test_make_pycurl_req(self):
+        curl_mock = mock.Mock()
+        curl_mock.getinfo.return_value = 'url'
+        with mock.patch('lib.pycurl.Curl', mock.Mock(return_value=curl_mock)):
+            self.assertEqual(lib.make_pycurl_request('url', None), ('', 'url'))
--- source/tests/test_utils.py
+++ source/tests/test_utils.py
@@ -27,6 +27,35 @@ class UtilsTestCase(unittest.TestCase):
                 with mock.patch('os.setsid', mock.Mock()):
                     utils.daemonize()
                     self.assertTrue(mock_exit.called)
+                    self.assertEqual(mock_exit.call_count, 1)
+
+    def test_daemonize_exc(self):
+        with mock.patch('os.fork', mock.Mock(side_effect=OSError(1, 'Hi!')), create=True):
+            with mock.patch('os._exit', mock.Mock()):
+                with mock.patch('os.setsid', mock.Mock()):
+                    with self.assertRaises(Exception):
+                        utils.daemonize()
+
+    def test_daemonize_pid0(self):
+        with mock.patch('os.fork', mock.Mock(return_value=0), create=True):
+            with mock.patch('os._exit', mock.Mock()) as mock_exit:
+                with mock.patch('os.setsid', mock.Mock()):
+                    utils.daemonize()
+                    self.assertEqual(mock_exit.call_count, 0)
+
+    def test_daemonize_pid0_exc(self):
+        with mock.patch('os.fork', mock.Mock(side_effect=[0, OSError(1, 'Bye')]), create=True):
+            with mock.patch('os._exit', mock.Mock()) as mock_exit:
+                with mock.patch('os.setsid', mock.Mock()):
+                    with self.assertRaises(Exception):
+                        utils.daemonize()
+
+    def test_daemonize_pid0_pidnot0(self):
+        with mock.patch('os.fork', mock.Mock(side_effect=[0, 7]), create=True):
+            with mock.patch('os._exit', mock.Mock(), create=True) as mock_exit:
+                with mock.patch('os.setsid', mock.Mock()):
+                    utils.daemonize()
+                    self.assertEqual(mock_exit.call_count, 1)
 
     def test_create_pidfile(self):
         pid = 42
@@ -45,11 +74,24 @@ class UtilsTestCase(unittest.TestCase):
 
     def test_get_tube(self):
         name_str = 'name'
-
-        class Queue():
-            def tube(self, name):
-                return name
-
-        queue = Queue()
-        with mock.patch('lib.utils.tarantool_queue.Queue', mock.Mock(return_value=queue)):
+        queue_mock = mock.Mock()
+        queue_mock.tube.return_value = name_str
+        with mock.patch('lib.utils.tarantool_queue.Queue', mock.Mock(return_value=queue_mock)):
             self.assertEqual(utils.get_tube(None, None, None, name_str), name_str)
+
+    def test_spawn_workers(self):
+        p_mock = mock.Mock()
+        process_mock = mock.Mock(return_value=p_mock)
+        with mock.patch('lib.utils.Process', process_mock):
+            utils.spawn_workers(7, None, None, 9)
+            self.assertEqual(process_mock.call_count, 7)
+            self.assertTrue(process_mock.daemon)
+            self.assertEqual(p_mock.start.call_count, 7)
+
+    def test_check_network_ok(self):
+        with mock.patch('lib.utils.urllib2.urlopen', mock.Mock()):
+            self.assertTrue(utils.check_network_status('url', 7))
+
+    def test_check_network_fail(self):
+        with mock.patch('lib.utils.urllib2.urlopen', mock.Mock(side_effect=ValueError)):
+            self.assertFalse(utils.check_network_status('url', 7))
