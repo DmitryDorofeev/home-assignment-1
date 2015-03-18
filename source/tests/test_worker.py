844403a0b61e430aafef3dff3e67f3b98a903d8f
Alexey Halaidzhy <a.halaidzhy@corp.mail.ru>
2015-03-18 08:33:49 +0300

    worker.get_redirect_history_from_task full coverage

--- source/tests/test_worker.py
+++ source/tests/test_worker.py
@@ -1,4 +1,4 @@
-__author__ = 'dmitry'
+__author__ = 'leshiy1295'
 
 import unittest
 import mock
@@ -9,11 +9,30 @@ class WorkerTestCase(unittest.TestCase):
 
     def test_get_redirect(self):
         to_unicode_mock = mock.Mock(return_value=u'url')
-        logger_mock = mock.Mock()
         task_mock = mock.Mock()
         task_mock.data = {'url': 'url', 'url_id': 7}
         with mock.patch('lib.worker.to_unicode', to_unicode_mock, create=True):
-            with mock.patch('lib.worker.logger', logger_mock, create=True):
-                is_input, data = get_redirect_history_from_task(task_mock, 10)
-                self.assertEqual(True, is_input)
+            with mock.patch('lib.worker.logger', mock.Mock(), create=True):
+                with mock.patch('lib.worker.get_redirect_history', mock.Mock(return_value=([], [], []))):
+                    is_input, data = get_redirect_history_from_task(task_mock, 10)
+                    self.assertEqual(False, is_input)
 
+    def test_get_redirect_with_suspicious(self):
+        to_unicode_mock = mock.Mock(return_value=u'url')
+        task_mock = mock.Mock()
+        task_mock.data = {'url': 'url', 'url_id': 7, 'suspicious': 'some text'}
+        with mock.patch('lib.worker.to_unicode', to_unicode_mock, create=True):
+            with mock.patch('lib.worker.logger', mock.Mock(), create=True):
+                with mock.patch('lib.worker.get_redirect_history', mock.Mock(return_value=([], [], []))):
+                    is_input, data = get_redirect_history_from_task(task_mock, 10)
+                    self.assertEqual(False, is_input)
+
+    def test_get_redirect_with_error(self):
+        to_unicode_mock = mock.Mock(return_value=u'url')
+        task_mock = mock.Mock()
+        task_mock.data = {'url': 'url', 'url_id': 7}
+        with mock.patch('lib.worker.to_unicode', to_unicode_mock, create=True):
+            with mock.patch('lib.worker.logger', mock.Mock(), create=True):
+                with mock.patch('lib.worker.get_redirect_history', mock.Mock(return_value=(['ERROR'], [], []))):
+                    is_input, data = get_redirect_history_from_task(task_mock, 10)
+                    self.assertEqual(True, is_input)
